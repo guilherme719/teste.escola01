af5a6ff711eda503db89f1a80cac200c4ef10ef3
Guilherme Henrique Fernandes Rosa Luccas <00001117845758sp@aluno.educacao.sp.gov.br>
2024-10-21 09:35:56 -0300

    Criado menu de opçoes

--- cadastro_funcionario.py
+++ cadastro_funcionario.py
@@ -1,31 +1,52 @@
-print("\n*****Cadastro De Funcionarios:***** ")
+print("### MENU ###")
 
-print("")
-print("")
+print("\nEscolha uma das opçoes abaixo: ")
 
-id = int(input("Digite a matricula: "))
-nome_completo = str(input("Digite o nome completo: "))
-cpf = int(input("Digite o CPF: "))
-profissao =str(input("DIgite a Profissão: "))
-area = str(input("Digite a Area: "))
-salario = float(input("Digite o Salario: "))
-bonificacao = float(input("Digite a Bonificaçao: "))
-salario_bruto = salario + bonificacao
+print(f"[1] PARA CADASTRO DO FUNCIONARIO ")
+print(f"[2] PARA CADASTRO DO CLIENTE")
+print(f"[3] PARA CADASTRO DE PRODUTO")
+print(f"[4] PARA SAIR")
 
-print("")
-print("")
+escolha = int(input("Escolha uma das opçoes: "))
 
-print("#####[CADASTRO DE FUNCIONÁRIOS]#####")
-print("") 
-print(f"Identificação do funcionario: [{id}]")
-print(f"Funcionario: [{nome_completo}]")
-print(f"CPF: [{cpf}]")
-print(f"Profissâo: [{profissao}]")
-print(f"Area: [{area}]")
-print(f"Salario: [{salario}]")
-print(f"Bonificação: [{bonificacao}]")
-print(f"Salario bruto: [{salario_bruto}]")
 
-print("")
-print("Fim do programa")
-print("")
+if escolha == 1:    
+
+    print("\n*****Cadastro De Funcionarios:***** ")
+
+    print("")
+    print("")
+
+    id = int(input("Digite a matricula: "))
+    nome_completo = str(input("Digite o nome completo: "))
+    cpf = int(input("Digite o CPF: "))
+    profissao =str(input("DIgite a Profissão: "))
+    area = str(input("Digite a Area: "))
+    salario = float(input("Digite o Salario: "))
+    bonificacao = float(input("Digite a Bonificaçao: "))
+    salario_bruto = salario + bonificacao
+
+    print("")
+    print("")
+
+    print("#####[CADASTRO DE FUNCIONÁRIOS]#####")
+    print("") 
+    print(f"Identificação do funcionario: [{id}]")
+    print(f"Funcionario: [{nome_completo}]")
+    print(f"CPF: [{cpf}]")
+    print(f"Profissâo: [{profissao}]")
+    print(f"Area: [{area}]")
+    print(f"Salario: [{salario}]")
+    print(f"Bonificação: [{bonificacao}]")
+    print(f"Salario bruto: [{salario_bruto}]")
+
+if escolha == 2:
+    print("Projeto em fase de desenvolvimento")
+    
+if escolha == 3:
+    print("Projeto em fase de desenvolvimento")
+    
+if escolha == 4:
+    print("")
+    print("Fim do programa")
+    print("")
